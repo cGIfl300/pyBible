413edf0975e534b32739d5293baf043412437c7f
cgifl300 <cgifl300@gmail.com>
2020-07-15 09:56:27 +0200

    secret-garden.py

---
+++ secret_garden.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright © 2020 cGIfl300
+#
+# This file contains secret data and should not be shared.
+
+db_type = 'sqlite'
+db_username = ''
+db_password = ''
+db_database = 'data/pybible.db'
+db_server = ''
+db_port = 3306
